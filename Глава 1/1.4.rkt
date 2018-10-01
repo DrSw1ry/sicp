076e895f863b9f9adbebb06967a88fb653f65487
DrSw1ry <gabesteam2@gmail.com>
2018-10-02 01:23:02 +0300

    Add files via upload

---
+++ Глава 1/1.4.rkt
@@ -0,0 +1,9 @@
+#lang racket
+;Определите процедуру, которая принимает в качестве аргументов три числа и возвращает сумму
+;квадратов двух больших из них.
+
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (more-or-less x y z) (cond ((and (> x y) (> z y)) (sum-of-squares x z))
+                                   ((and (> y x)  (> z x)) (sum-of-squares y z))
+                                   ((and (> x z)  (> y z)) (sum-of-squares x y)))  )                              
